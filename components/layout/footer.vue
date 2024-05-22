590abc33f3321477833e502bc5130b3ec862566f
Samuel Buendía <samuelb@keoworld.com>
2024-05-22 13:59:05 +0930

    add footer and alert components

---
+++ components/layout/footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-gray-300 dark:bg-gray-800 dark:text-white text-black text-center py-5 text-sm">
+        <span>Copyright © 2024 <a target="_blank" class="font-bold"
+                href="http://samuelbuendia.com">samuelbuendia.com</a></span><br>
+        <span>All Rights Reserved.</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div v-if="!isAuthenticated"
         class="min-h-[90vh] flex items-center justify-center px-4 sm:px-6 lg:px-8 h-5/6 w-4/6 mx-auto">
         <h1
-            class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800 text-center md:text-5xl lg:text-5xl">
+            class="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-800 text-center md:text-5xl lg:text-5xl">
             Do you want to discover all the products we have for you?
             <br><br>
             Log in to find out more...
@@ -12,7 +12,7 @@
         class="min-h-[90vh] flex items-center justify-center px-4 sm:px-6 lg:px-8 h-5/6 w-4/6 mx-auto">
         <h1
             class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800 text-center md:text-5xl lg:text-5xl">
-            Bienvenido {{ user?.name }}
+            Welcome {{ user?.name }}
             <br><br>
             <nuxt-link
                 class="lg:mr-2 transition duration-500 min-w-40 hover:scale-110 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
